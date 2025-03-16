66fceb247e90e619db3418931dc8baece78cf54e
diprefranco <39093878+diprefranco@users.noreply.github.com>
2025-03-16 18:08:30 -0300

    PostUseCaseTests: test AddPost call and user not exists.

--- SocialNetwork.Tests/Application/UseCases/PostUseCaseTests.cs
+++ SocialNetwork.Tests/Application/UseCases/PostUseCaseTests.cs
@@ -5,6 +5,7 @@ using SocialNetwork.Application.UseCases;
 using SocialNetwork.Application.Repositories.Interfaces;
 using SocialNetwork.Application.Repositories.DTO;
 using SocialNetwork.Application.UseCases.Interfaces;
+using SocialNetwork.Application.Exceptions;
 
 namespace SocialNetwork.Tests.Application.UseCases
 {
@@ -31,6 +32,49 @@ namespace SocialNetwork.Tests.Application.UseCases
             Assert.Equal(userName, post.UserName);
             Assert.Equal(content, post.Content);
             Assert.Equal(DateTime.Now, post.PostDateTime, new TimeSpan(0, 0, 1));
+        }
+
+        [Fact]
+        public void Execute_UserExistsAndPostContentValid_ShouldCallAddPostOnce()
+        {
+            //Arrange
+            const string userName = "Alfonso";
+            const string content = "Hola mundo";
+            Guid userId = Guid.NewGuid();
+
+            var mockUserRepository = new Mock<IUserRepository>();
+            mockUserRepository
+                .Setup(repo => repo.GetOneByUserName(userName))
+                .Returns(new UserDTO { Id = userId, UserName = userName });
+
+            IPostUseCase postUseCase = new PostUseCase(mockUserRepository.Object);
+
+            //Act
+            var post = postUseCase.Execute(userName, content);
+
+            //Assert
+            mockUserRepository.Verify(
+                repo => repo.AddPost(userId, content, It.IsAny<DateTime>()),
+                Times.Once
+            );
+        }
+
+        [Fact]
+        public void Execute_UserNotExistsAndPostContentValid_ShouldThrowUserNotFoundException()
+        {
+            //Arrange
+            const string userName = "Alfonso";
+            const string content = "Hola mundo";
+
+            var mockUserRepository = new Mock<IUserRepository>();
+            mockUserRepository
+                .Setup(repo => repo.GetOneByUserName(userName))
+                .Returns((UserDTO)null);
+
+            IPostUseCase postUseCase = new PostUseCase(mockUserRepository.Object);
+
+            //Act & Assert
+            Assert.Throws<UserNotFoundException>(() => postUseCase.Execute(userName, content));
         }
     }
 }
