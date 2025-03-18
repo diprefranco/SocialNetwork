82fcb0a5d3ced439a0d77f50d4983e7c8f54178e
diprefranco <39093878+diprefranco@users.noreply.github.com>
2025-03-18 00:22:21 -0300

    FollowUserUseCaseTests: Follower User Exists With Following added.

--- SocialNetwork.Tests/Application/UseCases/FollowUserUseCaseTests.cs
+++ SocialNetwork.Tests/Application/UseCases/FollowUserUseCaseTests.cs
@@ -1,24 +1,53 @@
 ﻿using Moq;
 using Xunit;
 using System;
+using System.Collections.Generic;
 using SocialNetwork.Application.Repositories.DTO;
 using SocialNetwork.Application.Repositories.Interfaces;
 using SocialNetwork.Application.UseCases.Interfaces;
 using SocialNetwork.Application.UseCases;
-using System.Collections.Generic;
 
 namespace SocialNetwork.Tests.Application.UseCases
 {
     public class FollowUserUseCaseTests
     {
         [Fact]
-        public void Execute_FollowerUserExistsWithEmptyFollowing_ShouldReturnFollowCreated()
+        public void Execute_FollowerUserExistsWithNoFollowing_ShouldReturnFollowCreated()
         {
             //Arrange
             const string followerUserName = "Alicia";
             const string followeeUserName = "Ivan";
             ICollection<UserDTO> following = new List<UserDTO>();
 
+            Execute_FollowerUserExists_ShouldReturnFollowCreated(followerUserName, followeeUserName, following);
+        }
+
+        [Fact]
+        public void Execute_FollowerUserExistsWithFollowing_ShouldReturnFollowCreated()
+        {
+            //Arrange
+            const string followerUserName = "Alicia";
+            const string followeeUserName = "Ivan";
+            ICollection<UserDTO> following = new List<UserDTO>
+            {
+                new UserDTO
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "Pedro"
+                },
+                new UserDTO
+                {
+                    Id = Guid.NewGuid(),
+                    UserName = "Juan"
+                }
+            };
+
+            Execute_FollowerUserExists_ShouldReturnFollowCreated(followerUserName, followeeUserName, following);
+        }
+
+        private void Execute_FollowerUserExists_ShouldReturnFollowCreated(string followerUserName, string followeeUserName, ICollection<UserDTO> following)
+        {
+            //Arrange
             var followerUser = new UserFollowingDTO { Id = Guid.NewGuid(), UserName = followerUserName, Following = following };
             var followeeUser = new UserDTO { Id = Guid.NewGuid(), UserName = followeeUserName };
 
@@ -29,7 +58,7 @@ namespace SocialNetwork.Tests.Application.UseCases
             mockUserRepository
                 .Setup(repo => repo.GetOneByUserName(followeeUserName))
                 .Returns(followeeUser);
-            
+
             IFollowUserUseCase followUserUseCase = new FollowUserUseCase(mockUserRepository.Object);
 
             //Act
